1e9f556bcf057a275504c88dcf89a1347a8b8bda
adela <adelas93@gmai.com>
2022-04-03 23:06:49 +0200

    rajout copy clipboard

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from random import choice
-
+import clipboard
 digits = '0123456789'
-chars = 'abcd3fgh1jklmn0pqrstuvwxyz'
+chars = 'a8cd3fgh1jk1mn0pqrstuvwxyz'
 up = chars.upper()
 specials = '!@$%&'
 All = digits + chars + up + specials
 
-password = ''.join(choice(All) for i in range(8))
-
+password = ''.join(choice(All) for i in range(10))
+clipboard.copy(password)
 print(password)
